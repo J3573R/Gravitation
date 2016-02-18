3539a6fe7209c0450c785c10c828e0c884189283
Tommi Hagelberg <tommihagelberg@gmail.com>
2016-02-18 19:59:07 +0200

    Initial commit

---
+++ android/src/fi/tamk/gravitation/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package fi.tamk.gravitation;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import fi.tamk.gravitation.gravitation;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new gravitation(), config);
+	}
+}
