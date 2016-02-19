2fcb47efe6251b12054001baa7e0ae45a6a49975
Tommi Hagelberg <tommihagelberg@gmail.com>
2016-02-19 14:10:03 +0200

    Player object

---
+++ core/src/fi/tamk/gravitation/GameLoop.java
@@ -0,0 +1,59 @@
+package fi.tamk.gravitation;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+
+/**
+ * Created by Clown on 18.2.2016.
+ */
+public class GameLoop extends ScreenAdapter {
+
+    MainGame game;
+    OrthographicCamera camera;
+    World world;
+    Player player;
+    Body ground;
+    Box2DDebugRenderer debug = new Box2DDebugRenderer();
+
+    public GameLoop(MainGame game){
+        this.game = game;
+
+        world = new World(new Vector2(0, -10f), true);
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 16, 9);
+        player = new Player(world);
+
+        BodyDef groundDef = new BodyDef();
+        groundDef.type = BodyDef.BodyType.StaticBody;
+        groundDef.position.set(camera.viewportWidth / 2, 0);
+
+        PolygonShape groundShape = new PolygonShape();
+        groundShape.setAsBox(camera.viewportWidth / 2, 0.1f);
+
+        ground = world.createBody(groundDef);
+
+        ground.createFixture(groundShape, 0);
+
+    }
+
+    @Override
+    public void render(float delta) {
+        game.batch.setProjectionMatrix(camera.combined);
+        camera.update();
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.batch.begin();
+        player.draw(game.batch);
+        game.batch.end();
+        debug.render(world, camera.combined);
+        world.step(1/60f, 8, 3);
+    }
+}
---
+++ core/src/fi/tamk/gravitation/Player.java
@@ -0,0 +1,41 @@
+package fi.tamk.gravitation;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.physics.box2d.*;
+
+/**
+ *
+ * Created by root on 19.2.2016.
+ */
+public class Player {
+    private World world;
+    private Texture img;
+    Body body;
+
+    public Player(World world){
+        this.world = world;
+        img = new Texture("badlogic.jpg");
+
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(16 / 2 - 0.5f, 9 / 2 - 0.5f);
+        body = world.createBody(bodyDef);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.density = 1f;
+        fixtureDef.restitution = 0.5f;
+        fixtureDef.friction = 0.2f;
+
+        PolygonShape box = new PolygonShape();
+        box.setAsBox(0.5f, 0.5f);
+
+        fixtureDef.shape = box;
+        body.createFixture(fixtureDef);
+
+    }
+
+    public void draw(SpriteBatch batch){
+        batch.draw(img, body.getPosition().x - 0.5f, body.getPosition().y - 0.5f, 1, 1);
+    }
+}
